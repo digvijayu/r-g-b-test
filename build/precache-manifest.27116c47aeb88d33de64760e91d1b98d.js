7a54f3cd8cbffa7f55d26b1d6a3b66d7f087bbd0
Digvijay Upadhyay <digvijay.upadhyay@globant.com>
2019-04-02 10:03:36 +0100

    committed the build folder

---
+++ build/precache-manifest.27116c47aeb88d33de64760e91d1b98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "706ee7936a0d9130ad39",
+    "url": "/static/css/main.c44bae6f.chunk.css"
+  },
+  {
+    "revision": "706ee7936a0d9130ad39",
+    "url": "/static/js/main.dc3f3285.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f4b466b20d379ad4b53",
+    "url": "/static/js/2.1116fd8d.chunk.js"
+  },
+  {
+    "revision": "0bf8bf42c0eca4692dddfe837eb760da",
+    "url": "/index.html"
+  }
+];
